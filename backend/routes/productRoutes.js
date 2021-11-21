2e88aca4a05b3c9717053c5d87e2b660f44f20a8
jackoyau <jackoyau555@gmail.com>
2021-11-22 01:49:47 +0800

    update before redux

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router= express.Router()
+import Product from '../models/productModel.js'
+
+//mongoose always gives out Promises,
+//and we use asyncHandler to simplify the try-catch in express
+
+// @desc   Fetch all products
+// @route  GET /api/products
+// @access Public
+router.get('/', asyncHandler(async(req,res)=>{ 
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+
+// @desc   Fetch single product
+// @route  GET /api/products/:id
+// @access Public
+router.get('/:id',asyncHandler(async(req,res)=>{
+    const product =  await Product.findById(req.params.id) //mongoose method
+    
+    if(product){
+        res.json(product)
+    } else {
+        res.status(404)  //.json({message:'Product not found'})
+        throw new Error ('Product not found')
+    }
+}))
+
+export default router
